a51d8c3744ad5149cf769a17bc04356f5471881d
Ilario Batistic <ilario.batistic@gmail.com>
2024-06-20 11:04:59 +0200

    inital commit

---
+++ src/main/java/com/kingict/kingictintership2024java/KingIctIntership2024JavaApplication.java
@@ -0,0 +1,13 @@
+package com.kingict.kingictintership2024java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingIctIntership2024JavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KingIctIntership2024JavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kingict/kingictintership2024java/KingIctIntership2024JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingict.kingictintership2024java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingIctIntership2024JavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
